52a2d59a73deab022662b1c6e64d6effbb10862b
John Calabrese <jcalabrese@pivotal.io>
2016-02-11 17:42:07 -0500

    [#113088669] vendoring in latest deps

--- Godeps/_workspace/src/github.com/pivotalservices/cfops/plugin/cfopsplugin/default_pivotalcf.go
+++ Godeps/_workspace/src/github.com/pivotalservices/cfops/plugin/cfopsplugin/default_pivotalcf.go
@@ -1,6 +1,18 @@
 package cfopsplugin
 
-import "github.com/pivotalservices/cfbackup"
+import (
+	"io"
+	"path"
+
+	"github.com/cloudfoundry-community/go-cfenv"
+	"github.com/pivotalservices/cfbackup"
+	"github.com/pivotalservices/cfops/tileregistry"
+)
+
+//GetHostDetails - return all of the host and archive details in the form of a tile spec object
+func (s *DefaultPivotalCF) GetHostDetails() tileregistry.TileSpec {
+	return s.TileSpec
+}
 
 //GetProducts - gets a products object from the given pivotalcf
 func (s *DefaultPivotalCF) GetProducts() (products map[string]cfbackup.Products) {
@@ -26,10 +38,25 @@ func (s *DefaultPivotalCF) GetCredentials() (creds map[string]map[string][]cfbac
 	return
 }
 
+//NewArchiveWriter - creates a writer to a named resource using the given name on the cfops defined target (s3, local, etc)
+func (s *DefaultPivotalCF) NewArchiveWriter(name string) (writer io.WriteCloser, err error) {
+	backupContext := cfbackup.NewBackupContext(s.TileSpec.ArchiveDirectory, cfenv.CurrentEnv())
+	writer, err = backupContext.StorageProvider.Writer(path.Join(s.TileSpec.ArchiveDirectory, name))
+	return
+}
+
+//NewArchiveReader - creates a reader to a named resource using the given name on the cfops defined target (s3, local, etc)
+func (s *DefaultPivotalCF) NewArchiveReader(name string) (reader io.ReadCloser, err error) {
+	backupContext := cfbackup.NewBackupContext(s.TileSpec.ArchiveDirectory, cfenv.CurrentEnv())
+	reader, err = backupContext.StorageProvider.Reader(path.Join(s.TileSpec.ArchiveDirectory, name))
+	return
+}
+
 //NewPivotalCF - creates the default pivotacf
-var NewPivotalCF = func(installationSettings *cfbackup.ConfigurationParser) PivotalCF {
+var NewPivotalCF = func(installationSettings *cfbackup.ConfigurationParser, ts tileregistry.TileSpec) PivotalCF {
 
 	return &DefaultPivotalCF{
+		TileSpec:             ts,
 		InstallationSettings: installationSettings,
 	}
 }
