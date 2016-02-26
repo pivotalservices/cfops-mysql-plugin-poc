0c6424542f0221b6566a986548ad82953980f998
Caleb Washburn <cwashburn@pivotal.io>
2016-02-26 10:35:10 -0700

    [#114487643] changes to use new InstallationSettings helpers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/pivotalservices/cfbackup"
@@ -20,28 +21,49 @@ func (s *MysqlPlugin) GetMeta() (meta cfopsplugin.Meta) {
 
 func (s *MysqlPlugin) Setup(pcf cfopsplugin.PivotalCF) (err error) {
 	s.PivotalCF = pcf
+	s.InstallationSettings = pcf.GetInstallationSettings()
 	return
 }
 
+func (s *MysqlPlugin) getSSHConfig() (sshConfig []command.SshConfig, err error) {
+	var IPs []string
+	var vmCredentials cfbackup.VMCredentials
+
+	if IPs, err = s.InstallationSettings.FindIPsByProductAndJob(productName, jobName); err == nil {
+		if vmCredentials, err = s.InstallationSettings.FindVMCredentialsByProductAndJob(productName, jobName); err == nil {
+			for _, ip := range IPs {
+				sshConfig = append(sshConfig, command.SshConfig{
+					Username: vmCredentials.UserID,
+					Password: vmCredentials.Password,
+					Host:     ip,
+					Port:     defaultSSHPort,
+					SSLKey:   vmCredentials.SSLKey,
+				})
+			}
+		}
+	}
+	return
+}
 func (s *MysqlPlugin) Backup() (err error) {
 	var writer io.WriteCloser
 	var persistanceBackuper cfbackup.PersistanceBackup
-	var sshConfig command.SshConfig
 	var mysqlUserName, mysqlPassword string
+	var sshConfigs []command.SshConfig
 
-	sshConfig, err = s.PivotalCF.GetSSHConfig(productName, jobName)
-	if err != nil {
-		return
-	}
+	if sshConfigs, err = s.getSSHConfig(); err == nil {
+		for _, sshConfig := range sshConfigs {
+			mysqlUserName, mysqlPassword, err = s.getMysqlCredentials()
+			if err != nil {
+				return
+			}
 
-	mysqlUserName, mysqlPassword, err = s.getMysqlCredentials()
-	if err != nil {
-		return
-	}
-	if persistanceBackuper, err = s.GetPersistanceBackup(mysqlUserName, mysqlPassword, sshConfig); err == nil {
-		if writer, err = s.PivotalCF.NewArchiveWriter(outputFileName); err == nil {
-			defer writer.Close()
-			err = persistanceBackuper.Dump(writer)
+			if persistanceBackuper, err = s.GetPersistanceBackup(mysqlUserName, mysqlPassword, sshConfig); err == nil {
+				if writer, err = s.PivotalCF.NewArchiveWriter(fmt.Sprintf(outputFileName, sshConfig.Host)); err == nil {
+					defer writer.Close()
+					err = persistanceBackuper.Dump(writer)
+				}
+			}
+
 		}
 	}
 	return
@@ -50,31 +72,32 @@ func (s *MysqlPlugin) Backup() (err error) {
 func (s *MysqlPlugin) Restore() (err error) {
 	var reader io.ReadCloser
 	var persistanceBackuper cfbackup.PersistanceBackup
-	var sshConfig command.SshConfig
 	var mysqlUserName, mysqlPassword string
 
-	sshConfig, err = s.PivotalCF.GetSSHConfig(productName, jobName)
-	if err != nil {
-		return
-	}
+	var sshConfigs []command.SshConfig
 
-	mysqlUserName, mysqlPassword, err = s.getMysqlCredentials()
-	if err != nil {
-		return
-	}
+	if sshConfigs, err = s.getSSHConfig(); err == nil {
+		for _, sshConfig := range sshConfigs {
 
-	if persistanceBackuper, err = s.GetPersistanceBackup(mysqlUserName, mysqlPassword, sshConfig); err == nil {
-		if reader, err = s.PivotalCF.NewArchiveReader(outputFileName); err == nil {
-			defer reader.Close()
-			err = persistanceBackuper.Import(reader)
+			mysqlUserName, mysqlPassword, err = s.getMysqlCredentials()
+			if err != nil {
+				return
+			}
+			if persistanceBackuper, err = s.GetPersistanceBackup(mysqlUserName, mysqlPassword, sshConfig); err == nil {
+				if reader, err = s.PivotalCF.NewArchiveReader(fmt.Sprintf(outputFileName, sshConfig.Host)); err == nil {
+					defer reader.Close()
+					err = persistanceBackuper.Import(reader)
+				}
+			}
 		}
 	}
+
 	return
 }
 
 const (
 	pluginName                 = "mysql-tile"
-	outputFileName             = pluginName + ".dmp"
+	outputFileName             = pluginName + "_%s.dmp"
 	productName                = "p-mysql"
 	jobName                    = "mysql"
 	mysqlCredentialsName       = "mysql_admin_password"
@@ -95,14 +118,14 @@ func NewMysqlPlugin() *MysqlPlugin {
 
 type MysqlPlugin struct {
 	PivotalCF            cfopsplugin.PivotalCF
+	InstallationSettings cfbackup.InstallationSettings
 	Meta                 cfopsplugin.Meta
 	GetPersistanceBackup func(string, string, command.SshConfig) (cfbackup.PersistanceBackup, error)
 }
 
 func (s *MysqlPlugin) getMysqlCredentials() (userName, pwd string, err error) {
 	var props map[string]string
-	props, err = s.PivotalCF.GetPropertyValues(productName, jobName, mysqlCredentialsName)
-	if err == nil {
+	if props, err = s.InstallationSettings.FindPropertyValues(productName, jobName, mysqlCredentialsName); err == nil {
 		userName = props[identityName]
 		pwd = props[passwordName]
 	}
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -29,59 +30,66 @@ var _ = Describe("Given MysqlPlugin", func() {
 			})
 		})
 	})
-
-	Describe("given a Backup() method", func() {
-		Context("when called on a properly setup mysqlplugin object", func() {
-			var err error
-			fakePersistenceBackup := new(FakePersistenceBackup)
-			var controlTmpDir string
-			BeforeEach(func() {
-				controlTmpDir, _ = ioutil.TempDir("", "unit-test")
-				mysqlplugin = &MysqlPlugin{
-					Meta: cfopsplugin.Meta{
-						Name: "mysql-tile",
-					},
-					GetPersistanceBackup: func(user, pass string, config command.SshConfig) (pb cfbackup.PersistanceBackup, err error) {
-						return fakePersistenceBackup, nil
-					},
-				}
-				pivotalCF := cfopsplugin.NewPivotalCF(cfbackup.NewConfigurationParser("./fixtures/installation-settings-1-6-aws.json"), tileregistry.TileSpec{
-					ArchiveDirectory: controlTmpDir,
-				})
-				mysqlplugin.Setup(pivotalCF)
-				err = mysqlplugin.Backup()
-			})
-
-			AfterEach(func() {
-				os.RemoveAll(controlTmpDir)
-			})
-
-			It("then it should dump the target mysql contents", func() {
-				Ω(fakePersistenceBackup.DumpCallCount).Should(Equal(1))
-			})
-
-			It("then it should create an archive file", func() {
-				Ω(IsEmpty(controlTmpDir)).ShouldNot(BeTrue())
-			})
-		})
-	})
-
-	Describe("given a Setup() method", func() {
-		Context("when called with a PivotalCF containing a MySQL tile", func() {
-			var pivotalCF cfopsplugin.PivotalCF
-			BeforeEach(func() {
-				pivotalCF = cfopsplugin.NewPivotalCF(cfbackup.NewConfigurationParser("./fixtures/installation-settings-1-6-aws.json"), tileregistry.TileSpec{})
-				mysqlplugin.Setup(pivotalCF)
-			})
-
-			It("then it should extract Mysql username required for backup/restore", func() {
-				Ω(mysqlplugin.PivotalCF).ShouldNot(BeNil())
-			})
-
-		})
-	})
-
+	testInstallationSettings("./fixtures/installation-settings-1-6-aws.json")
 })
+
+func testInstallationSettings(installationSettingsPath string) {
+	var mysqlplugin *MysqlPlugin
+	Describe(fmt.Sprintf("given a installationSettingsFile %s", installationSettingsPath), func() {
+		Describe("given a Backup() method", func() {
+			Context("when called on a properly setup mysqlplugin object", func() {
+				var err error
+				fakePersistenceBackup := new(FakePersistenceBackup)
+				var controlTmpDir string
+				BeforeEach(func() {
+					controlTmpDir, _ = ioutil.TempDir("", "unit-test")
+					mysqlplugin = &MysqlPlugin{
+						Meta: cfopsplugin.Meta{
+							Name: "mysql-tile",
+						},
+						GetPersistanceBackup: func(user, pass string, config command.SshConfig) (pb cfbackup.PersistanceBackup, err error) {
+							return fakePersistenceBackup, nil
+						},
+					}
+					configParser := cfbackup.NewConfigurationParser(installationSettingsPath)
+					pivotalCF := cfopsplugin.NewPivotalCF(configParser.InstallationSettings, tileregistry.TileSpec{
+						ArchiveDirectory: controlTmpDir,
+					})
+					mysqlplugin.Setup(pivotalCF)
+					err = mysqlplugin.Backup()
+				})
+
+				AfterEach(func() {
+					os.RemoveAll(controlTmpDir)
+				})
+
+				It("then it should dump the target mysql contents", func() {
+					Ω(fakePersistenceBackup.DumpCallCount).Should(Equal(1))
+				})
+
+				It("then it should create an archive file", func() {
+					Ω(IsEmpty(controlTmpDir)).ShouldNot(BeTrue())
+				})
+			})
+		})
+
+		Describe("given a Setup() method", func() {
+			Context("when called with a PivotalCF containing a MySQL tile", func() {
+				var pivotalCF cfopsplugin.PivotalCF
+				BeforeEach(func() {
+					configParser := cfbackup.NewConfigurationParser(installationSettingsPath)
+					pivotalCF = cfopsplugin.NewPivotalCF(configParser.InstallationSettings, tileregistry.TileSpec{})
+					mysqlplugin.Setup(pivotalCF)
+				})
+
+				It("then it should extract Mysql username required for backup/restore", func() {
+					Ω(mysqlplugin.PivotalCF).ShouldNot(BeNil())
+				})
+
+			})
+		})
+	})
+}
 
 func IsEmpty(name string) bool {
 	f, err := os.Open(name)
