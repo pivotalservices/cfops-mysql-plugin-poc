959217db0905815a14eba5aa342b19d523a213f9
John Calabrese <jcalabrese@pivotal.io>
2016-04-06 15:38:47 -0400

    [#117088577] removing godeps

--- Godeps/_workspace/src/github.com/pivotalservices/cfops/plugin/cfopsplugin/init.go
+++
@@ -1,9 +0,0 @@
-package cfopsplugin
-
-import "encoding/gob"
-
-func init() {
-	gob.Register(make(map[string]interface{}))
-	gob.Register(new(DefaultPivotalCF))
-	gob.Register(new(BackupRestoreRPC))
-}
